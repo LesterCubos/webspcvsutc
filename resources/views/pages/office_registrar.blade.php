c7b32ae561e67676391999b636abe4f258e8a4e6
LesterCubos <lester.cubos@cvsu.edu.ph>
2023-04-26 13:02:17 +0800

    test

---
+++ resources/views/pages/office_registrar.blade.php
@@ -0,0 +1,56 @@
+<!-- ======= Ocr Section ======= -->
+<section id="ocr" class="ocr">
+    <div class="container" data-aos="fade-up">
+
+      <ul class="nav nav-tabs row  g-2 d-flex">
+
+        <li class="nav-item col-3">
+          <a class="nav-link active show" data-bs-toggle="tab" data-bs-target="#tab-1">
+            <h4>Instruction</h4>
+          </a>
+        </li><!-- End tab nav item -->
+
+        <li class="nav-item col-3">
+          <a class="nav-link" data-bs-toggle="tab" data-bs-target="#tab-2">
+            <h4>Organizational chart</h4>
+          </a><!-- End tab nav item -->
+
+      </ul>
+
+      <div class="tab-content">
+
+        <div class="tab-pane active show" id="tab-1">
+          <div class="row">
+            <div class="col flex-column justify-content-center" data-aos="fade-up" data-aos-delay="100">
+              <h3>Voluptatem dignissimos provident</h3>
+              <p class="fst-italic">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                magna aliqua.
+              </p>
+              <ul>
+                <li><i class="bi bi-check2-all"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+                <li><i class="bi bi-check2-all"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+                <li><i class="bi bi-check2-all"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+              </ul>
+            </div>
+          </div>
+        </div><!-- End tab content item -->
+
+        <div class="tab-pane" id="tab-2">
+          <div class="row">
+            <div class="col d-flex flex-column justify-content-center">
+              <h3>Neque exercitationem debitis</h3>
+              <p class="fst-italic">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                magna aliqua.
+              </p>
+              <br>
+              <img src="assets/img/kazuha.jpg" alt="" class="img-fluid">
+            </div>
+          </div>
+        </div><!-- End tab content item -->
+
+      </div>
+
+    </div>
+  </section><!-- End Ocr Section -->
