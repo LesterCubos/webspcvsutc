cc966bd25d5a14e0d4d2c68ded1e8e801495a31e
LesterCubos <lester.cubos@cvsu.edu.ph>
2023-05-15 11:12:44 +0800

    Organization

---
+++ app/Http/Controllers/Webpage/AboutOrgsController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers\Webpage;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
+
+use App\Models\AboutOrgs;
+
+use App\Http\Requests\AboutOrgs\StoreRequest;
+use App\Http\Requests\AboutOrgs\UpdateRequest;
+class AboutOrgsController extends Controller
+{
+    public function index(): Response
+    {
+        return response()->view('about_orgs.index', [
+            'about_orgs' => AboutOrgs::orderBy('updated_at', 'desc')->get(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): Response
+    {
+        return response()->view('about_orgs.form');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRequest $request): RedirectResponse
+    {
+        $validated = $request->validated();
+
+
+        // insert only requests that already validated in the StoreRequest
+        $create = AboutOrgs::create($validated);
+
+        if($create) {
+            // add flash for the success notification
+            session()->flash('notif.success', 'Organization registered successfully!');
+            return redirect()->route('about_orgs.index');
+        }
+
+        return abort(500);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id): Response
+    {
+        return response()->view('about_orgs.show', [
+            'about_org' => AboutOrgs::findOrFail($id),
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id): Response
+    {
+        return response()->view('about_orgs.form', [
+            'about_org' => AboutOrgs::findOrFail($id),
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRequest $request, string $id): RedirectResponse
+    {
+        $about_orgs = AboutOrgs::findOrFail($id);
+        $validated = $request->validated();
+
+        $update = $about_orgs->update($validated);
+
+        if($update) {
+            session()->flash('notif.success', 'Organization updated successfully!');
+            return redirect()->route('about_orgs.index');
+        }
+
+        return abort(500);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id): RedirectResponse
+    {
+        $about_org = AboutOrgs::findOrFail($id);
+
+        $delete = $about_org->delete($id);
+
+        if($delete) {
+            session()->flash('notif.success', 'Organization deleted successfully!');
+            return redirect()->route('about_orgs.index');
+        }
+
+        return abort(500);
+    }
+}
+
---
+++ resources/views/pages/nonacademic_orgs.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.show')
+
+@section('content')
+
+  <!-- ======= Breadcrumbs ======= -->
+  <div class="breadcrumbs">
+    <div class="bread d-flex align-items-center"></div>
+    <nav>
+      <div class="container">
+        <ol>
+          <li><a href="/"><i class='bx bxs-home'></i> Home</a></li>
+          <li>Student Affairs</li>
+          <li>Non- Academic Organizations</li>
+        </ol>
+      </div>
+    </nav>
+  </div><!-- End Breadcrumbs -->
+
+<!-- ======= CSG Section ======= -->
+<section id="csg" class="csg">
+    <div class="container" data-aos="fade-up">
+
+      <div class="section-title">
+        <h3>Central<span> Student Government</span></h3>
+      </div>
+      @foreach ($csgs as $csg)
+      <div class="info">
+        <h2>{{ $csg->title }}</h2>
+        <p>{!! $csg->content !!}
+        </p>
+      </div>
+
+      <div class="section-title">
+        <h3>CSG Members</span></h3>
+      </div>
+
+      <table class="table table-bordered table-hover">
+        <thead class="table-dark">
+          <tr>
+            <th scope="col">NAME</th>
+            <th scope="col">POSITION</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">{{ $csg->name }}</th>
+            <td>{{ $csg->position }}</td>
+          </tr>
+        </tbody>
+      </table>
+      @endforeach
+    </div>
+  </section><!-- End CSG Section -->
+
+@endsection
