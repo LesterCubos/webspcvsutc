fcf4d8ffcc0fb5084f32006bea189234c94f49fe
Jeromedelrosario13 <jerome.delrosario@cvsu.edu.ph>
2023-10-06 20:24:09 +0800

    commit sp footer

--- resources/views/superadmin/sp/sp_superadmin_footer.blade.php
+++ resources/views/superadmin/sp/sp_superadmin_footer.blade.php
@@ -1,11 +1,6 @@
 <footer class="footer" style="background-color: #3c4252">
     <div class="d-sm-flex justify-content-center justify-content-sm-between">
-<<<<<<< HEAD
-      <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
-      <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"><a href="/" target="_blank">Cavite State University-Tanza Campus</a>. All Rights Reserved <?php echo date("Y.");?></span>
-=======
       <span class=" d-block text-center text-sm-left d-sm-inline-block" style="color: #fff">Copyright © bootstrapdash.com 2020</span>
       <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center" style="color: #fff"><a href="/" target="_blank">Cavite State University Tanza Campus</a>. All Rights Reserved <?php echo date("Y.");?></span>
->>>>>>> cda591a65ad4852b34a63a6340e3ef98aa7e42e1
     </div>
 </footer>
