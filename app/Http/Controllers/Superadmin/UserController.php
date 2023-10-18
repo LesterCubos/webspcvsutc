728c08b678805624446bdd5f0c6857b5cb6fe855
Jeromedelrosario13 <jerome.delrosario@cvsu.edu.ph>
2023-10-18 17:56:58 +0800

    remove unused codes

--- app/Http/Controllers/Superadmin/UserController.php
+++ app/Http/Controllers/Superadmin/UserController.php
@@ -5,12 +5,8 @@ namespace App\Http\Controllers\Superadmin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Response;
-use App\Http\Requests\User\StoreRequest;
-use App\Http\Requests\User\UpdateRequest;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Requests\ProfileUpdateRequest;
 class UserController extends Controller
 {
     // public function __construct(){
@@ -27,42 +23,5 @@ class UserController extends Controller
     public function form(){
         // $users = User::all();
         return view('superadmin.sp.manage_user_pages.form');
-    }
-
-    /**
-     * Update the user's profile information.
-     */
-    public function update(ProfileUpdateRequest $request): RedirectResponse
-    {
-        $request->user()->fill($request->validated());
-
-        if ($request->user()->isDirty('email')) {
-            $request->user()->email_verified_at = null;
-        }
-
-        $request->user()->save();
-
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
-    }
-
-    /**
-     * Delete the user's account.
-     */
-    public function destroy(Request $request): RedirectResponse
-    {
-        $request->validateWithBag('userDeletion', [
-            'password' => ['required', 'current_password'],
-        ]);
-
-        $user = $request->user();
-
-        Auth::logout();
-
-        $user->delete();
-
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
-
-        return Redirect::to('/');
-    }
+    }   
 }
