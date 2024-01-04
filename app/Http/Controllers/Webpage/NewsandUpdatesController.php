e3271b2236b5c48b06149e16896eebad5137e9ea
Jeromedelrosario13 <jerome.delrosario@cvsu.edu.ph>
2024-01-04 23:16:46 +0800

    fixed contact infos at news

--- app/Http/Controllers/Webpage/NewsandUpdatesController.php
+++ app/Http/Controllers/Webpage/NewsandUpdatesController.php
@@ -9,6 +9,7 @@ use App\Models\CarouselItem;
 use App\Models\SocialMediaLinks;
 use App\Models\QuickLinks;
 use App\Models\OtherLinks;
+use App\Models\ContactInfo;
 
 use Illuminate\Http\Request;
 
@@ -25,11 +26,12 @@ class NewsandUpdatesController extends Controller
     $totalVisits=views(CarouselItem::class)->count();
 
     $quicks = QuickLinks::all();
-        $others = OtherLinks::all();
-        $socialmedias = SocialMediaLinks::all();
+    $others = OtherLinks::all();
+    $socialmedias = SocialMediaLinks::all();
+    $contact_infos = ContactInfo::all();
 
     $news = News::all();
-    return view('pages.news.news', compact('news','totalViews','totalVisits','quicks','others','socialmedias'), ['new' => $new]);
+    return view('pages.news.news', compact('contact_infos','news','totalViews','totalVisits','quicks','others','socialmedias'), ['new' => $new]);
     }
 
 }
